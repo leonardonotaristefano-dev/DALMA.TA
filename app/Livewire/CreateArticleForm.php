bef05abea7e8a7d6055097cccb160e1644f2beb1
Angelo Alvisi <angeloalvisi@gmail.com>
2025-04-18 10:53:22 +0200

    User Story 1 completata

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Article;
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Auth;
 
 class CreateArticleForm extends Component
