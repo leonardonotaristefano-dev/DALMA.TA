fa4d593f7603f550f12642562b20556452be2523
Dario Hendrix <dariohendrix@hotmail.it>
2025-04-24 10:46:36 +0200

    inizio user story 10

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ git pull
 php artisan migrate
 php artisan app:make-user-revisor -tuamail-
 
+
+
 collegare il .env a riga 50 con il proprio mailTrap:
  esempio
  MAIL_MAILER=smtp
@@ -11,3 +13,15 @@ MAIL_HOST=sandbox.smtp.mailtrap.io
 MAIL_PORT=2525
 MAIL_USERNAME=codicediMailTrap
 MAIL_PASSWORD=codicediMailTrap
+
+composer require laravel/scout
+
+aggiungi in env e in env.example:
+SCOUT_DRIVER=tntsearch
+
+php artisan scout:flush "App\Models\Article"
+php artisan scout:import "App\Models\Article"
+
+in gitignore:
+(riga 1)
+/storage/*.index
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Category;
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
-{
+{use Searchable;
     protected $fillable = [
         'title',
         'description',
@@ -36,4 +37,14 @@ class Article extends Model
     public static function toBeRevisionedCount(){
         return Article::where('is_accepted', null)->count();
     }
+
+    public function toSearchableArray(){
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'category' => $this->category,
+        ];
+    }
+    
 }
